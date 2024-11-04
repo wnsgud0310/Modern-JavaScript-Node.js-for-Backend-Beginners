5cd6f85244518c8f56ce45fecda5c962359e902b
wnsgud0310 <ffjjo0310@naver.com>
2024-11-04 16:47:50 +0900

    js null 실습

---
+++ ch3/null.js
@@ -0,0 +1,8 @@
+// null 값
+// null 은 빈 값을 의미한다. 존재하지 않는다고 의도적으로 표현하는 것
+
+var love = null; // love라는 변수에 null 값을 대입해서 변수에 값이 비어있음을 나타냄
+console.log(love);
+console.log(Boolean(love)); // null 값인 변수 love를 Boolean() 함수를 통해 불린형으로 표현하면 false가 출력된다
+
+
