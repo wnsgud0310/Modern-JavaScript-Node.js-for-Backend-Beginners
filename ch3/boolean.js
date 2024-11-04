5ced30e09948a5bb48b8275d6c1f7bc3d21ee857
wnsgud0310 <ffjjo0310@naver.com>
2024-11-04 16:37:25 +0900

    js 불리언형 실습

---
+++ ch3/boolean.js
@@ -0,0 +1,13 @@
+//변수 이름 사용 규칙
+// 불리언형(Boolean)은 논리 요소를 나타내며 true와 false 2가지의 값을 가질 수 있다.
+
+var checkIn = true;
+var checkOut = false;
+console.log(checkIn);
+console.log(checkOut);
+
+// 불리언형은 참과 거짓을 판별하는 타입이므로, 주로 비교문에서 많이 사용됨
+var bigger = 100 > 50;
+console.log(bigger); // true
+bigger = 100 < 50;
+console.log(bigger); // false
