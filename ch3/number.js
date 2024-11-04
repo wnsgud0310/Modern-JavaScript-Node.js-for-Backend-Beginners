a6624c3717d59e636a267136d4559673814f4b9a
wnsgud0310 <ffjjo0310@naver.com>
2024-11-04 16:22:30 +0900

    js 숫자형 실습

---
+++ ch3/number.js
@@ -0,0 +1,15 @@
+// var myAge = 20;
+// var myHeight = 178.5;
+// console.log(typeof myAge);
+// console.log(typeof myHeight);
+
+// var kuk = 95;
+// var eng = 85;
+// var math = 52;
+// var total = kuk + eng + math;
+// var avg = total / 3;
+// console.log(total);
+// console.log(avg);
+
+var nan = "난" / 10;
+console.log(nan);
---
+++ ch3/variable.js
@@ -0,0 +1,4 @@
+var msg = "Hello";
+var count = 100;
+msg = count; // 변수끼리의 복사
+console.log(msg);
